35c77d0b3a101accdf19f94094abc553fe0f3873
Johns Jose <jjkanichukattu@gmail.com>
2025-03-07 00:50:41 +0530

    Delete temp.c

--- temp.c
+++
@@ -1,8 +0,0 @@
-//Reverse a number
-#include <stdio.h>
-int main() {
-    int num = 1234, rev = 0;
-    while (num > 0) { rev = rev * 10 + num % 10; num /= 10; }
-    printf("Reversed: %d\n", rev);
-    return 0;
-}
